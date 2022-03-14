e91b77025cb59f90e519c17dd51bf3e2f401ab9f
Sakeena Khalifa <p2602192@dmu.ac.uk>
2022-03-14 15:46:21 +0000

    I have started to add test properties to my tables. I am not yet done. I had made sure there were no errors before pushing back to the branch.

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,63 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        private string nName = "random name";
+
+
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            Assert.IsNotNull(Acustomer);
+        }
+        [TestMethod]
+        public void NameOK()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            string TestData = nName;
+            Acustomer.Name = TestData;
+            Assert.AreEqual(Acustomer.Name, TestData);
+
+        }
+
+        [TestMethod]
+        public void ValidOK()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            string Error = "";
+            string TestData = nName;
+            Error = Acustomer.Valid(nName);
+            Acustomer.Name = TestData;
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void MinLessOne()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            string Error = "";
+            string TestData = "";
+            Error = Acustomer.Valid(TestData);
+            Acustomer.Name = TestData;
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void MaxPlusOne()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            string Error = "";
+            string TestData = "";
+            TestData = TestData.PadLeft(51, '*');
+            Error = Acustomer.Valid(TestData);
+            Acustomer.Name = TestData;
+            Assert.AreNotEqual(Error, "");
+        }
+    }
+}
